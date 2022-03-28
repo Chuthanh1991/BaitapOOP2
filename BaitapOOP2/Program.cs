26f7d88ea9a8e10e8d9315803bb8023cee410497
Chuthanh1991 <chukimthanh1@gmail.com>
2022-03-28 15:51:24 +0700

    Add project files.

---
+++ BaitapOOP2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaitapOOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            QLTV chucnang = new QLTV();
+            int maTL;
+            string tenNhaXB;
+            int soBanPH;
+            string tenTG;
+            int soTrang;
+            int SoPH;
+            DateTime ThangPH;
+            DateTime NgayPH;
+            chucnangsach chucnangsach = new chucnangsach();
+            do
+            {
+                Console.WriteLine("Chọn chức năng");
+                Console.WriteLine("1. Quản lý các tài liệu Sách");
+                Console.WriteLine("2. Quản lý các tài liệu Tạp chí");
+                Console.WriteLine("3. Quản lý các tài liệu Báo");
+                string chon = Console.ReadLine();
+                switch (chon)
+                {
+                    case "1":
+                        do
+                        {
+                            
+                            Console.WriteLine("a. THÊM Sách");
+                            Console.WriteLine("b. XOÁ Sách");
+                            Console.WriteLine("c. CẬP NHẬT Sách");
+                            Console.WriteLine("d. TÌM KIẾM Sách");
+                            Console.WriteLine("e. HIỆN THỊ DANH SÁCH");
+                            string chon1 = Console.ReadLine();
+                            switch (chon1)
+                            {
+                                case "a":
+                                    Console.WriteLine("Nhập Mã Sách:");
+                                    maTL = Int32.Parse(Console.ReadLine());
+                                    Console.WriteLine("Nhập Tên nhà Xuất Bản:");
+                                    tenNhaXB = Console.ReadLine();
+                                    Console.WriteLine("Nhập số phát hành :");
+                                    soBanPH = Int32.Parse(Console.ReadLine());
+                                    Console.WriteLine("Nhập tên tác giả:");
+                                    tenTG = Console.ReadLine();
+                                    Console.WriteLine("Nhập số trang:");
+                                    soTrang = Int32.Parse(Console.ReadLine());
+                                    Sach sach = new Sach();
+                                    sach.MaTL = maTL;
+                                    sach.TenNhaXB = tenNhaXB;
+                                    sach.SoBanPH = soBanPH;
+                                    sach.TenTG = tenTG;
+                                    sach.SoTrang = soTrang;
+                                    chucnangsach.AddSach(sach);
+                                    break;
+                                case "b":
+                                    Console.WriteLine("Nhập Mã Sách:");
+                                    maTL = Int32.Parse(Console.ReadLine());
+                                    chucnangsach.DeleteSach(maTL);
+                                    break;
+                                case "c":
+                                    Console.WriteLine("Nhập Mã Sách:");
+                                    maTL = Int32.Parse(Console.ReadLine());
+                                    Console.WriteLine("Nhập Tên nhà Xuất Bản:");
+                                    tenNhaXB = Console.ReadLine();
+                                    Console.WriteLine("Nhập số phát hành :");
+                                    soBanPH = Int32.Parse(Console.ReadLine());
+                                    Console.WriteLine("Nhập tên tác giả:");
+                                    tenTG = Console.ReadLine();
+                                    Console.WriteLine("Nhập số trang:");
+                                    soTrang = Int32.Parse(Console.ReadLine());
+                                    chucnangsach.UpdateSach(maTL, tenNhaXB, soBanPH, tenTG, soTrang);
+                                    break;
+                                case "d":
+                                    Console.WriteLine("Nhập Mã Sách:");
+                                    maTL = Int32.Parse(Console.ReadLine());
+                                    chucnangsach.Search(maTL);
+                                    break;
+                                case "e":
+                                  chucnangsach.hienthi();
+                                    break;
+                            }
+                        }
+                        while (true);
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ BaitapOOP2/QLTV.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaitapOOP2
+{
+    public class QLTV
+    {
+        public int MaTL { get; set; }
+        public string TenNhaXB { get; set; }
+        public int SoBanPH { get; set; }
+        public void In()
+        {
+            Console.WriteLine("Mã tài liệu: {0} -- Tên nhà Xuất Bản: {1}-- Số xuất bản: {2}", MaTL, TenNhaXB, SoBanPH);
+        }
+
+    }
+
+    public class Sach :QLTV
+    {
+        public string TenTG { get; set; }
+        public int SoTrang { get; set; }
+        public void Insach()
+        {
+            Console.WriteLine("Mã tài liệu: {0}  -- Tên nhà Xuất Bản: {1}-- Số xuất bản: {2}--Tên tác giả: {3}-- Số trang: {4}", MaTL, TenNhaXB, SoBanPH, TenTG, SoTrang);
+        }
+    }
+  
+    public class Tapchi : QLTV
+    {
+        public int SoPH { get; set; }
+        public DateTime ThangPH { get; set; }
+        public void In()
+        {
+            Console.WriteLine("Mã tài liệu: {0} -- Số phát hành: {1} -- Tên nhà Xuất Bản: {2}-- Số xuất bản: {3} -- tháng phát hành: {4}", MaTL, SoPH, TenNhaXB, SoBanPH, ThangPH);
+        }
+    }
+    public class Bao : QLTV
+    {
+        public DateTime NgayPH { get; set; }
+        public void In()
+        {
+            Console.WriteLine("Mã tài liệu: {0} -- Tên nhà Xuất Bản: {1}-- Số xuất bản: {2}-- Ngày phát hành: {3}", MaTL, TenNhaXB, SoBanPH, NgayPH);
+        }
+    }
+    public class chucnangsach
+    {
+        ArrayList Ds = new ArrayList();
+        public void AddSach(Sach S)
+        {
+            Ds.Add(S);
+        }
+        public void DeleteSach(int maTL)
+        {
+            Sach sach = new Sach();
+            foreach (Sach S in Ds)
+            {
+                if (maTL == S.MaTL)
+                {
+                    sach = S;
+                }
+            }
+            Ds.Remove(sach);
+        }
+        public void UpdateSach(int maTL, string tenNhaXB, int soBanPH, string tenTG, int soTrang)
+        {
+            foreach (Sach S in Ds)
+            {
+                if (S.MaTL == maTL)
+                {
+                    S.TenNhaXB = tenNhaXB;
+                    S.SoBanPH = soBanPH;
+                    S.TenTG = tenTG;
+                    S.SoTrang = soTrang;
+                }
+            }
+        }
+        public Sach Search(int maTL)
+        {
+            Sach sach = new Sach();
+            foreach (Sach S in Ds)
+            {
+                if (S.MaTL == maTL)
+                {
+                    sach = S;
+                }
+            }
+            return sach;
+        }
+        public void hienthi()
+        {
+            foreach (Sach S in Ds)
+            {
+                S.Insach();
+            }
+        }
+    }
+}
